c16030b20719459a905871374a9fdfa73acf85b7
Miguel Alberto Ordoñez <miguelssj5040@gmail.com>
2023-12-17 18:50:00 +0000

    Proyecto modeling

--- src/models.py
+++ src/models.py
@@ -7,26 +7,70 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base): # parent
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, nullable=False, )
+    name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(100), nullable=False)
+    users = relationship("Favorites")
+
+class Favorites(Base): # children
+    __tablename__ = 'favorites'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    # Relacion para usuarios
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship("User", back_populates="users")
+    # Relacion para characters
+    id_characters = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship("Characters")
+    # Relacion para planets
+    id_planets = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship("Planets")
+    # Relacion para vehicles
+    id_vehicles = Column(Integer, ForeignKey('vehicles.id'))
+    vehicles = relationship("Vehicles")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    type = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    birth_year = Column(Integer)
+    gender = Column(String(20))
+    height = Column(Integer)
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
 
-    def to_dict(self):
-        return {}
+class Planets(Base):
+    __tablename__ = 'planets'
 
-## Draw from SQLAlchemy base
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    diameter = Column(Integer)
+    gravity = Column(String(100))
+    population = Column(Integer)
+    climate = Column(String(100))
+    terrain = Column(String(100))
+    favorites = relationship("Favorites")
+
+class Vehicles(Base):
+    __tablename__ ='vehicles'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    model = Column(String(100))
+    vehicle_class = Column(String(100))
+    passengers = Column(Integer)
+    manufacturer = Column(String(100))
+    length = Column(Integer)
+
+
+
 render_er(Base, 'diagram.png')
